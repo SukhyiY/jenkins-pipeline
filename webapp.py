38ce4f5316e2bd8f14e8a5b53bbf712cc2da4f45
Yaroslav Sukhyi <ysukhy@macmini4417.ddns.softservecom.com>
2019-05-16 12:12:45 +0300

    Helm config added

--- webapp.py
+++ webapp.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Simple text message to you!"
+    image_tag = os.environ.get('app-version')
+    return render_template('index.html', image_tag=image_tag)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
