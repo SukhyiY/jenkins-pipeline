f5a60cea877e3c5c8c6622ef21bb27f18363ae75
Yaroslav Sukhyi <47447416+SukhyiY@users.noreply.github.com>
2019-05-31 14:45:07 +0300

    test image

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, image
 import os
 app = Flask(__name__)
 
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def hello():
     image_tag = os.environ.get('app_version')
     return render_template('index.html', image_tag=image_tag)
+    return image('Flask-logo.png')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
